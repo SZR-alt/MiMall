2a0aa7e80590156d4fec71454d164a0bb00e4cef
SZR-alt <2062279899@qq.com>
2020-10-07 00:45:32 +0800

    拦截待定

--- src/storage/index.js
+++ src/storage/index.js
@@ -28,10 +28,12 @@ export default {
     clear(key, module_name) {
         let val = this.getStorage();
         if (module_name) {
+            if (!val[module_name]) return;
             delete val[module_name][key];
         } else {
             delete val[key];
         }
-        this.setItem(STORAGE_KEY, val);
+        // this.setItem(STORAGE_KEY, val);
+        window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify(val));
     }
 }
