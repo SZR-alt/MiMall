4377b047da7f9a36ee5cbedfd8c94df42a664b87
SZR-alt <2062279899@qq.com>
2020-10-07 18:32:59 +0800

    商品首页

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ export default {
   data(){
     return{
       // age:30
-      res:{}
+      // res:{}
     }
   },
   mounted(){
@@ -43,20 +43,13 @@ export default {
     //   this.res=res;
     // });
     // 本地集成mockjs实现数据mock
-    this.axios.get('/user/login').then((res)=>{
-      this.res=res;
-    });
+    // this.axios.get('/user/login').then((res)=>{
+    //   this.res=res;
+    // });
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './assets/scss/reset.scss'
 </style>
