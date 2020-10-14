5df01359cf910757471661050dbe3d980f8509f6
SZR-alt <2062279899@qq.com>
2020-10-14 14:59:19 +0800

    商品首页—Modal动画实现

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -81,7 +81,7 @@
                                     <div class="item-info">
                                         <h3>{{item.name}}</h3>
                                         <p>{{item.subtitle}}</p>
-                                        <p class="price">{{item.price}}元</p>
+                                        <p class="price" @click="addCart(item.id)">{{item.price}}元</p>
                                     </div>
                                 </div>
                             </div>
@@ -90,7 +90,7 @@
                 </div>   
             </div>
         <service-bar></service-bar>
-        <modal title="提示" sureText="查看购物车" btnType="1" modalType="middle" v-bind:showModal="true">
+        <modal title="提示" sureText="查看购物车" btnType="1" modalType="middle" v-bind:showModal="showModal" v-on:submit="goToCart" v-on:cancel="showModal=false">
             <template v-slot:body>
                 <p>商品添加成功</p>
             </template>
@@ -196,7 +196,8 @@ export default {
                     img:'/imgs/ads/ads-4.jpg'
                 }
             ],
-            phoneList:[]
+            phoneList:[],
+            showModal:false
         }
     },
     mounted(){
@@ -214,6 +215,21 @@ export default {
                 res.list=res.list.slice(6,14);
                 this.phoneList=[res.list.slice(0,4),res.list.slice(4,8)]
             })
+        },
+        addCart(){
+            this.showModal=true;
+            // return;
+            /*this.axios.post('/carts',{
+                productId:id,
+                selected:true
+            }).then(()=>{
+
+            }).catch(()=>{
+                this.showModal=true;
+            })*/
+        },
+        goToCart(){
+            this.$router.push('/cart');
         }
     }
 }
